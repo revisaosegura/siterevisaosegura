ab81fdd57d1b5e62ef23defa51f54f1907d44dce
Revisao Segura <segurarevisao@gmail.com>
2025-02-27 22:26:04 -0300

    Correções no acesso ao banco antes das migrações

--- revisao_segura/asgi.py
+++ revisao_segura/asgi.py
@@ -1,11 +1,12 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
+from django.core.management import call_command
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "revisao_segura.settings")
+
 django.setup()
 
-from django.core.management import call_command
 call_command("migrate")  # Roda as migrações automaticamente
 
 application = get_asgi_application()
