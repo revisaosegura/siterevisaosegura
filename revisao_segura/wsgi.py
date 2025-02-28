c534b0786f7f2d63c80b398fb79278eaf7be225c
Revisao Segura <segurarevisao@gmail.com>
2025-02-27 21:54:05 -0300

    Forçando migrações no startup

--- revisao_segura/wsgi.py
+++ revisao_segura/wsgi.py
@@ -1,6 +1,11 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "revisao_segura.settings")
+django.setup()
+
+from django.core.management import call_command
+call_command("migrate")  # Roda as migrações automaticamente
 
 application = get_wsgi_application()
