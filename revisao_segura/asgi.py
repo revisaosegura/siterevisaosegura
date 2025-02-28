2d60538b76d141eac5467f67a3669af314099764
Revisao Segura <segurarevisao@gmail.com>
2025-02-27 21:55:51 -0300

    Forçando migrações no startup

--- revisao_segura/asgi.py
+++ revisao_segura/asgi.py
@@ -1,6 +1,11 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "revisao_segura.settings")
+django.setup()
+
+from django.core.management import call_command
+call_command("migrate")  # Roda as migrações automaticamente
 
 application = get_asgi_application()
