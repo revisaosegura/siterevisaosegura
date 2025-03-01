61d315678b9273e7b072b5c3e4170838da9325c1
Revisao Segura <segurarevisao@gmail.com>
2025-02-28 22:38:13 -0300

    Corrigindo configuração de wsgi.py

--- revisao_segura/wsgi.py
+++ revisao_segura/wsgi.py
@@ -15,21 +15,7 @@ application = get_wsgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'revisao_segura.settings')
 django.setup()
 
-from django.contrib.auth import get_user_model
-
-def reset_admin_password():
-    User = get_user_model()
-    username = "revisaosegura"  # Substitua pelo seu usuário admin
-    new_password = "Admin.2025"  # Nova senha
-
-    try:
-        user = User.objects.get(username=username)
-        user.set_password(new_password)
-        user.save()
-        print(f"Senha do superusuário '{username}' foi redefinida com sucesso!")
-    except User.DoesNotExist:
-        print(f"O usuário '{username}' não existe.")
-
-reset_admin_password()
+call_command('migrate')
+print("Migrações aplicadas com sucesso!")
 
 application = get_wsgi_application()
